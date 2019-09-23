3a6e1cc37b90465f5e9b1295c6d4c3109ae8a609
Satria Aji Putra <satriamaxt@gmail.com>
2019-09-23 19:14:25 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan10/logika/PBO210118068Latihan10Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Latihan membuat program logika dengan variable boolean
+ * serta operator logika >-=
+ */
+public class PBO210118068Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
